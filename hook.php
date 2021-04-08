906d596ad1502a23b9390ed990c3e99ab6598375
HellFinger-s <62233619+HellFinger-s@users.noreply.github.com>
2021-04-08 10:38:39 +0300

    Add files via upload

--- hook.php
+++ hook.php
@@ -2,12 +2,12 @@
 require 'vendor/autoload.php';
 $data = file_get_contents('php://input');
 $data = json_decode($data, true);
-$api = new Binance\API("lBmye1phscDHMWNiQycfDi5n8ENfjMWFZAe8fwgtUtwuqidFsaPe5qi1KtouGcdg","gA5s7pq4ubYCRiAyD6DidtCguhOyD1I8vJtmDKqKAzYjHWEmhLSFf2BfmorAAiaV");
+$api = new Binance\API();// ключи API
 $condition = false;
 $count = 0;
 $pares = ["BNBUSDT", "ETHUSDT", "BCHUSDT"];// пары, с которыми я работаю | 
 if (empty($data['message']['chat']['id'])) {
-	exit();
+  exit();
 }
 $message = $data["message"];
 $chat_id = 435322357;
@@ -27,27 +27,23 @@ switch($text) {
                 
         }
         break;
-            
-    case "/stopWork":
-        $condition = true;
-        break;
     default:
 }
 
 function sendMessage($chat_id, $text) {// функция отправки сообщения пользователю | 
-    $response = array
-    (
-		'chat_id' => $chat_id,
-		'text' => $text
-	);
-	$ch = curl_init('https://api.telegram.org/bot1184926146:AAEgmCHlMRVn-2tXy765OkIKxj-8OcB-PBQ/sendMessage');  
-	curl_setopt($ch, CURLOPT_POST, 1);  
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	$res = curl_exec($ch);
-	curl_close($ch);
-	return $res;
+  $response = array
+  (
+    'chat_id' => $chat_id,
+    'text' => $text
+  );
+  $ch = curl_init('https://api.telegram.org/botBotToken/sendMessage');  
+  curl_setopt($ch, CURLOPT_POST, 1);  
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  $res = curl_exec($ch);
+  curl_close($ch);
+  return $res;
 }
 function work(string $pareName, $chat_id, $api) {
     $old_price = file_get_contents(__DIR__ . '/'.$pareName.'price.txt');// получаем старую цену пары
@@ -69,8 +65,8 @@ function work(string $pareName, $chat_id, $api) {
     {
         if($price > $old_price*$max_multiplier)// если текущая цена выше желаемой 
         {
-            $quantity = $balances[substr($pareName, 0, -4)]["available"];
-            sendMessage($chat_id, "__________");
+            $quantity = $balances[substr($pareName, 0, -4)]["available"];// получаем количество монет для продажи
+            sendMessage($chat_id, "__________");// сообщение о неудачной продаже
             sendMessage($chat_id, "___success sell".$pareName);
             sendMessage($chat_id, "___buy price ".$old_price." sell price ". $price);
             sendMessage($chat_id, "___balance ".$quantity*$price);
@@ -80,26 +76,32 @@ function work(string $pareName, $chat_id, $api) {
         }
         else
         {
-            sendMessage($chat_id, $pareName." failed sell, now price: ".$price." < wanted price: ". $old_price*$max_multiplier);
+            sendMessage(
+               $chat_id,
+               $pareName." failed sell, now price: ".$price." < wanted price: ". $old_price*$max_multiplier
+               );// сообщение о неудачной продаже
         }
     }
     else// пытаемся купить монету 
     {
-        if($price < $min_price*$min_multiplier && $balances["USDT"]["available"] >= 12)
+        if($price < $min_price*$min_multiplier && $balances["USDT"]["available"] >= 12)// если цена ниже желаемой и на балансе есть минимальная сумма для покупк
         {
-            $quantity = round(12/$price, $accuracy);
+            $quantity = round(12/$price, $accuracy);// получаем количество монет для покупки
             
-            sendMessage($chat_id, "__________");
+            sendMessage($chat_id, "__________");// сообщение об успешной покупке
             sendMessage($chat_id,"___success buy".$pareName);
             sendMessage($chat_id,"___buy price ". $price." minim ".$min_price);
             sendMessage($chat_id,"___quantity ". $quantity);
-            $order = $api->marketBuy($pareName, $quantity);
+            $order = $api->marketBuy($pareName, $quantity);// покупаем
             file_put_contents(__DIR__ . '/'.$pareName.'price.txt', $price);
             file_put_contents(__DIR__ . '/'.$pareName. 'moveType.txt', "sell");
         }
         else
         {
-            sendMessage($chat_id, $pareName." failed buy, now price: ".$price." > wanted price: ". $min_price*$min_multiplier." balance ".$balances["USDT"]["available"]." minim ".$min_price. " saved minim ".$saved_min_price);
+            sendMessage(
+              $chat_id,
+              $pareName." failed buy, now price: ".$price." > wanted price: ". $min_price*$min_multiplier." balance ".$balances["USDT"]["available"]." minim ".$min_price. " saved minim ".$saved_min_price
+              );// сообщение о неудачной покупке
         }
     }
 }
